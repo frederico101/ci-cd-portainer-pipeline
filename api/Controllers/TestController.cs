a435b02c06bf24cbb19a276f9256a80e4cfd3290
frederico alves <fredyalves.fredyalves@hotmail.com>
2024-09-10 22:41:28 -0300

    test 1

---
+++ api/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "Get")]
+    public IActionResult Get()
+    {
+        return Ok("hi");
+    }
+}
